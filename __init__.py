d105a2915b0bd33785ac5a4bc77ef3f9db50e75c
dvfer <diego.pablo23@gmail.com>
2023-12-23 22:32:20 -0300

    init update

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ from .get_data import (
     get_type_agents)
 
 from .IA import send_prompt_openai
+from .IA import send_prompt_llama2, load_model
+
 
 from .post import (Post, 
                   INSTRUCTIONS_TEMPLATE, 
