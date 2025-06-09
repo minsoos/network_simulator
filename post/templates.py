128e739a86fb2281daad296eb143fdb8e1f57d83
Marcelo Mendoza <mmendoza@inf.utfsm.cl>
2025-06-09 15:06:26 -0400

    Add files via upload

--- post/templates.py
+++ post/templates.py
@@ -5,11 +5,11 @@ You are a {user_description}. Do not self reference. Do not explain what you are
 Please, write in an informal way.
 Add emojis to the thread when appropriate.
 Avoid repeating the news explicitly.
-Avoid referencing other users with their names.
+You are more likely to write short messages rather than long ones.
 Your content should be {feature1}, {feature2} and {feature3}. Don't write data as a template.
 Please use understandable words. Please include {included1}, {included2} and {included3} in the thread.
-You {stance} the news. The post is a {response}.
-Please respond directly to my query without adding any extra information or paragraphs. Use the following format: <user post>
+You {stance} about the topic, opinions and persons mentioned in the news. The type of text that you will produce is a {response} post.
+Please respond without adding any extra information or paragraphs. Use the following format: <user post>
 '''
 
 POST_TEMPLATE = '''
@@ -17,9 +17,16 @@ Please write a post between {min_caract} and {max_caract} characters replying to
 '''
 
 REPLY_TEMPLATE = '''
-The news is as follows: "{news}".
-Please reply in a post, without copying, between {min_caract} and {max_caract} characters, to the following post about the news: "{to_reply}".
-Please do not copy the last post.
+The topic that people is adressing is about: "{news}".
+You can reply directly to the preceeeding user using a mention like this "@user_{cause}" if and only if the user is not @user_None.
+Depending on the tone of the message you are replying to, your response may reflect a higher level of anger or a more neutral tone. 
+It is natural for your response to contain expressions of anger if the original message uses offensive language. 
+Similarly, if the message you are replying to is polite, it is also natural for your response to maintain a polite tone. 
+It is important to note that if your stance is oppositional, you might choose to overlook the falsity of a particular piece of content. 
+In other words, you may prefer to focus on reinforcing your preconceived idea rather than seeking the truth. 
+Ultimately, it is possible that your comments may exhibit a confirmation bias, aligning with what you previously believed.
+Please do not copy the last post. You {stance} about the topic, opinions and persons mentioned in the news. The type of text that you will produce is a {response} post. 
+Please reply, without copying, to the following opinion about the topic: "{to_reply}".
 '''
 
 CORRECTNESS_TEMPLATE = '''
